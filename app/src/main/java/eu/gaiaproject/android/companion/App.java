860709122ad72ec5580475d7c657726e3a6c4ca8
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2019-01-25 13:19:48 +0200

    add splunk mint

--- app/src/main/java/eu/gaiaproject/android/companion/App.java
+++ app/src/main/java/eu/gaiaproject/android/companion/App.java
@@ -3,6 +3,7 @@ package eu.gaiaproject.android.companion;
 import android.content.ContextWrapper;
 
 import com.pixplicity.easyprefs.library.Prefs;
+import com.splunk.mint.Mint;
 
 import androidx.multidex.MultiDexApplication;
 import static eu.gaiaproject.android.companion.util.Constants.SHRD_PREF_SERVICES_NAME;
@@ -21,6 +22,8 @@ public class App extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         mInstance = this;
+
+        Mint.initAndStartSession(getApplicationContext(), "2d901b25");
 
         // initialize the Prefs class
         new Prefs.Builder()
